a0922339797f015c17413d5a55d59c7e42fe7da4
Christian Reall-Fluharty <creallfluharty@gmail.com>
2022-12-04 12:49:29 -0600

    day 4

---
+++ day_4/solution_part_1.py
@@ -0,0 +1,7 @@
+with open("input.txt") as f:
+    pairs = 0
+    for line in f:
+        l1, r1, l2, r2 = (int(num) for a_range in line.split(',') for num in a_range.split('-'))
+        pairs += (l1 <= l2 <= r2 <= r1 or l2 <= l1 <= r1 <= r2)
+
+    print(pairs)
---
+++ day_4/solution_part_2.py
@@ -0,0 +1,7 @@
+with open("input.txt") as f:
+    pairs = 0
+    for line in f:
+        l1, r1, l2, r2 = (int(num) for a_range in line.split(',') for num in a_range.split('-'))
+        pairs += (l2 <= r1 and l1 <= r2)
+
+    print(pairs)
